ddd7811627870e60a29eb8cb5cd6597231c2a69e
TJ <62259677+Pixelz22@users.noreply.github.com>
2023-07-22 19:47:05 -0400

    Mobility and Offense v1

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -8,17 +8,54 @@ public class MyBot : IChessBot
         return moves[0];
     }
 
+    int[] POINT_VALUES = new int[] { 100, 350, 350, 525, 1000 };
+
     public int Evaluate(Board board)
     {
+        // Material
         PieceList[] pieceLists = board.GetAllPieceLists();
-        int material = (pieceLists[0].Count - pieceLists[6].Count) * 100
-            + (pieceLists[1].Count - pieceLists[7].Count) * 350
-            + (pieceLists[2].Count - pieceLists[8].Count) * 350
-            + (pieceLists[3].Count - pieceLists[9].Count) * 525
-            + (pieceLists[4].Count - pieceLists[10].Count) * 1000;
+        int material = (pieceLists[0].Count - pieceLists[6].Count) * POINT_VALUES[0]
+            + (pieceLists[1].Count - pieceLists[7].Count) * POINT_VALUES[1]
+            + (pieceLists[2].Count - pieceLists[8].Count) * POINT_VALUES[2]
+            + (pieceLists[3].Count - pieceLists[9].Count) * POINT_VALUES[3]
+            + (pieceLists[4].Count - pieceLists[10].Count) * POINT_VALUES[4];
         material *= (board.IsWhiteToMove) ? 1 : -1;
 
+        // Mobility
+        int mobility = 0;
+        int offense = 0;
+        foreach (Move move in board.GetLegalMoves())
+        {
+            PieceType movingType = move.MovePieceType;
+            PieceType capturedType = move.CapturePieceType;
+            switch (movingType)
+            {
+                case PieceType.Pawn:
+                    mobility += 100;
+                    break;
+                case PieceType.Knight:
+                    mobility += 350;
+                    break;
+                case PieceType.Rook:
+                    mobility += 250;
+                    break;
+                case PieceType.Bishop:
+                    mobility += 300;
+                    break;
+                case PieceType.Queen:
+                    mobility += 500;
+                    break;
+            }
 
-        return material;
+            offense += POINT_VALUES[(int)capturedType] * 2 - POINT_VALUES[(int)movingType];
+        }
+        
+        // Vulnerability
+
+        
+        // King Safety
+
+
+        return material + mobility + offense;
     }
 }
