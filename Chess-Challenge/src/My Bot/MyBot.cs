c108a4bfa9ce14c20610c64595e761c3cadca7cb
TJ <62259677+Pixelz22@users.noreply.github.com>
2023-07-22 19:23:26 -0400

    Material Evaluatation

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -7,4 +7,18 @@ public class MyBot : IChessBot
         Move[] moves = board.GetLegalMoves();
         return moves[0];
     }
+
+    public int Evaluate(Board board)
+    {
+        PieceList[] pieceLists = board.GetAllPieceLists();
+        int material = (pieceLists[0].Count - pieceLists[6].Count) * 100
+            + (pieceLists[1].Count - pieceLists[7].Count) * 350
+            + (pieceLists[2].Count - pieceLists[8].Count) * 350
+            + (pieceLists[3].Count - pieceLists[9].Count) * 525
+            + (pieceLists[4].Count - pieceLists[10].Count) * 1000;
+        material *= (board.IsWhiteToMove) ? 1 : -1;
+
+
+        return material;
+    }
 }
